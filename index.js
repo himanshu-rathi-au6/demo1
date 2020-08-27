14a625ca6993294efa4dc4fabd811b524f4f9689
Himanshu Rathi <himanshubrathi@gmail.com>
2020-08-27 16:22:11 +0530

    added new file

---
+++ index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+
+app.get('/',function(req,res){
+  res.send('hello World')
+})
+
+app.listen(8080,function(){
+  console.log('Server started')
+  console.log('new linw')
+})
